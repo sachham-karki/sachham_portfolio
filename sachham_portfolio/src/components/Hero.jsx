35303d9d8c9e3dff02b55c27ece096b90298ff9f
SachKar <88773679+sachham-karki@users.noreply.github.com>
2023-08-26 16:32:36 +0930

    Hero section debugged

--- sachham_portfolio/src/components/Hero.jsx
+++ sachham_portfolio/src/components/Hero.jsx
@@ -16,18 +16,18 @@ const Hero = () => {
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className='text-[#915EFF]'>Adrian</span>
+            Hi, I'm <span className='text-[#915EFF]'>Sachham</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I develop 3D visuals, user <br className='sm:block hidden' />
-            interfaces and web applications
+            I am Full-Stack Web Developer <br className='sm:block hidden' />
+            Cloud & SEO Specialist. 
           </p>
         </div>
       </div>
 
       <ComputersCanvas />
 
-      <div className='flex justify-center items-center'>
+      <div className='absolute xs:bottom-5 bottom-32 w-full flex justify-center items-center'>
         <a href='#about'>
           <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
             <motion.div
