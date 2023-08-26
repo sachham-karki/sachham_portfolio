1ae9e4fd70c2b6f63bc649f5cd56cbf35225f0c1
SachKar <88773679+sachham-karki@users.noreply.github.com>
2023-08-26 20:06:12 +0930

    config file updated

--- sachham_portfolio/vite.config.js
+++ sachham_portfolio/vite.config.js
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'sachham_portfolio', // Change 'your-repo-name' to your GitHub repository name
+  base: '/your-repo-name/', // Change 'your-repo-name' to your GitHub repository name
   build: {
-    outDir: 'dist', // Change 'dist' to the desired build output directory
+    outDir: 'build', // Change 'dist' to the desired build output directory
   },
   plugins: [react()],
 })
