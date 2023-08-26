7788cfe401751b25b1a9e84654e8fe201b5b1fc8
SachKar <88773679+sachham-karki@users.noreply.github.com>
2023-08-26 19:16:23 +0930

    vite config updated for amplify

--- sachham_portfolio/vite.config.js
+++ sachham_portfolio/vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'sachham_portfolio', // Change 'your-repo-name' to your GitHub repository name
+  build: {
+    outDir: 'dist', // Change 'dist' to the desired build output directory
+  },
   plugins: [react()],
 })
