76af0e08162a16b782b048303ec1d13eb5762aa1
SachKar <88773679+sachham-karki@users.noreply.github.com>
2023-08-28 22:40:13 +0930

    Adding node modules

--- sachham_portfolio/src/assets/index.js
+++ sachham_portfolio/src/assets/index.js
@@ -21,11 +21,15 @@ import redux from "./tech/redux.png";
 import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
+import wordpress from "./tech/wordpress.png";
 
 import meta from "./company/meta.png";
 import shopify from "./company/shopify.png";
 import starbucks from "./company/starbucks.png";
 import tesla from "./company/tesla.png";
+import vci from "./company/vci.jpg";
+import fap from "./company/fap.jpg";
+import sharon from "./company/sharon.jpg";
 
 import carrent from "./carrent.png";
 import jobit from "./jobit.png";
@@ -62,4 +66,8 @@ export {
   carrent,
   jobit,
   tripguide,
+  wordpress,
+  vci,
+  fap,
+  sharon,
 };
--- sachham_portfolio/src/constants/index.js
+++ sachham_portfolio/src/constants/index.js
@@ -23,6 +23,10 @@ import {
     jobit,
     tripguide,
     threejs,
+    wordpress,
+    vci,
+    fap,
+    sharon,
   } from "../assets";
   
   export const navLinks = [
@@ -42,19 +46,19 @@ import {
   
   const services = [
     {
-      title: "Web Developer",
+      title: "Full-Stack Developer",
       icon: web,
     },
     {
-      title: "React Native Developer",
+      title: "Wordpress Developer & SEO Specialist",
       icon: mobile,
     },
     {
-      title: "Backend Developer",
+      title: "AWS/Azure Cloud Architech",
       icon: backend,
     },
     {
-      title: "Content Creator",
+      title: "Content Creator/ Designer",
       icon: creator,
     },
   ];
@@ -96,10 +100,7 @@ import {
       name: "MongoDB",
       icon: mongodb,
     },
-    {
-      name: "Three JS",
-      icon: threejs,
-    },
+  
     {
       name: "git",
       icon: git,
@@ -112,28 +113,34 @@ import {
       name: "docker",
       icon: docker,
     },
+     {
+      name: "Wordpress",
+      icon: wordpress,
+    },
+    
   ];
   
   const experiences = [
     {
-      title: "React.js Developer",
-      company_name: "Starbucks",
-      icon: starbucks,
+      title: "Full Stack Developer & SEO Specialist",
+      company_name: "FirstAidPro",
+      icon: fap,
       iconBg: "#383E56",
-      date: "March 2020 - April 2021",
+      date: "Jan 2023 - Present",
       points: [
-        "Developing and maintaining web applications using React.js and other related technologies.",
-        "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-        "Implementing responsive design and ensuring cross-browser compatibility.",
-        "Participating in code reviews and providing constructive feedback to other developers.",
+        "- Proficient in utilizing WordPress (Elementor and DIVI) to create visually appealing, mobile responsive, and user-friendly websites.",
+        "- Extensive knowledge and expertise in utilizing a wide range of WordPress plugins.",
+        "- Skilled in writing custom code using a variety of languages, including HTML, CSS, Javascript (JQuery), and PHP, to enhance website functionality and design.",
+        "- Well-versed in implementing effective SEO strategies, encompassing On-Page, Off-page, and Technical SEO techniques, to drive organic traffic and improve search engine rankings.",
+        "- Possess valuable 'sales experience', delivering exceptional results through strong service knowledge, effective communication, and relationship-building skills.",
       ],
     },
     {
-      title: "React Native Developer",
-      company_name: "Tesla",
-      icon: tesla,
+      title: "Full Stack Developer",
+      company_name: "Victorian Chamber Of Commerce & Industry",
+      icon: vci,
       iconBg: "#E6DEDD",
-      date: "Jan 2021 - Feb 2022",
+      date: "Jan 2022 - Aug 2022",
       points: [
         "Developing and maintaining web applications using React.js and other related technologies.",
         "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
@@ -172,27 +179,27 @@ import {
   const testimonials = [
     {
       testimonial:
-        "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
-      name: "Sara Lee",
-      designation: "CFO",
-      company: "Acme Co",
-      image: "https://randomuser.me/api/portraits/women/4.jpg",
+        "After Sachham optimized our website, our traffic increased by 50%. We can't thank him enough!",
+      name: "Sharon McCulloch",
+      designation: "CEO",
+      company: "FirstAidPro",
+      image: sharon,
     },
     {
       testimonial:
-        "I've never met a web developer who truly cares about their clients' success like Rick does.",
-      name: "Chris Brown",
-      designation: "COO",
-      company: "DEF Corp",
+        "I thought it was impossible to make a website as beautiful as our product, but Sachham proved me wrong.",
+      name: "Bonnie Gambhir",
+      designation: "CFO",
+      company: "NJM Group",
       image: "https://randomuser.me/api/portraits/men/5.jpg",
     },
     {
       testimonial:
-        "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
-      name: "Lisa Wang",
-      designation: "CTO",
-      company: "456 Enterprises",
-      image: "https://randomuser.me/api/portraits/women/6.jpg",
+        "I've never met a web developer who truly cares about their company' success like Sachham does.",
+      name: "Lisa Christiansen",
+      designation: "Manager",
+      company: "Labax Nominees",
+      image: "https://randomuser.me/api/portraits/women/4.jpg",
     },
   ];
   
